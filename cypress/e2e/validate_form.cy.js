149272234f868b83afee33512b438813497a76eb
kaszkietow <pbulanda642@gmail.com>
2025-03-10 15:43:32 +0100

    Updated cypress test

--- cypress/e2e/validate_form.cy.js
+++ cypress/e2e/validate_form.cy.js
@@ -3,9 +3,44 @@ describe("Testowy formularz", () => {
       cy.visit("http://localhost:3000")
     })
 
-    it('Nie wysyła formularza gdy pola są puste', () =>{
+    it('Nie wysyła formularza gdy wszystkie pola są puste', () =>{
         cy.get('button').click()
 
         cy.contains('Dziękujemy za przesłanie formularza!').should('not.exist');
-    }) 
+    })
+
+    it('Nie wysyła formularza gdy email i age są puste', () =>{
+        cy.get('#name').type("Piotrek");
+        
+        cy.get('button').click()
+
+        cy.contains('Dziękujemy za przesłanie formularza!').should('not.exist');
+    })
+
+    it('Nie wysyła formularza gdy pole age jest puste', () =>{
+        cy.get('#name').type("Piotrek");
+        cy.get('#email').type("piotrek@gmail.com");
+        
+        cy.get('button').click()
+
+        cy.contains('Dziękujemy za przesłanie formularza!').should('not.exist');
+    })
+
+    it('Nie wysyła formularza gdy pole e-mail jest puste', () =>{
+        cy.get('#name').type("Piotrek");
+        cy.get('#age').type("23");
+        
+        cy.get('button').click()
+
+        cy.contains('Dziękujemy za przesłanie formularza!').should('not.exist');
+    })
+
+    it('Nie wysyła formularza gdy pole name jest puste', () =>{
+        cy.get('#email').type("piotrek@gmail.com");
+        cy.get('#age').type("23");
+        
+        cy.get('button').click()
+
+        cy.contains('Dziękujemy za przesłanie formularza!').should('not.exist');
+    })
 })
